1a0b60bf0582bd6b7a08aac448fc002c2e5af049
Alex <alexmitchelldev@gmail.com>
2020-07-23 14:21:33 -0500

    Dynamic add/delete LIs

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,9 +1,61 @@
 function addTodo() {
+	//DOM Variable declarations
 	let todoText = document.getElementById('newTodoText').value,
-		todoList = document.getElementById('todoUl'),
+		dailyList = document.getElementById('dailyList'),
+		weeklyList = document.getElementById('weeklyList'),
+		specificList = document.getElementById('specificList'),
+		todoList = document.querySelector('input[name="todoType"]:checked').value,
 		newTodoLi = document.createElement('LI'),
+		newSpan = document.createElement('span'),
+		newIcon = document.createElement('i'),
 		txtNode = document.createTextNode(todoText);
 
-	newTodoLi.appendChild(txtNode);
-	todoList.appendChild(newTodoLi);
+	//Adds font aswesome classes to icon
+	newIcon.classList.add('fa', 'fa-trash');
+	newSpan.addEventListener('click', function determineParent() {
+		if (event.target.parentElement.nodeName === 'LI') {
+			event.target.parentElement.remove();
+			// console.log('You clicked on the span');
+		} else if (event.target.parentElement.nodeName === 'SPAN');
+		{
+			event.target.parentElement.parentElement.remove();
+			// console.log('You clicked on the icon');
+		}
+	});
+
+	function addInfo() {
+		newTodoLi.appendChild(newSpan);
+		newTodoLi.appendChild(txtNode);
+		newSpan.appendChild(newIcon);
+	}
+
+	//Logic to choose which todo list to add to
+	if (todoList == 'dailyList') {
+		addInfo();
+		dailyList.appendChild(newTodoLi);
+	} else if (todoList === 'weeklyList') {
+		addInfo();
+		weeklyList.appendChild(newTodoLi);
+	} else if (todoList === 'specificList') {
+		addInfo();
+		specificList.appendChild(newTodoLi);
+	}
 }
+
+function removeTodo(event) {}
+
+// function removeTodo() {
+// 	let todos = document.querySelectorAll('#dailyList li'),
+// 		tab = [],
+// 		liIndex;
+// 	for (var i = 0; i < todos.length; i++) {
+// 		tab.push(todos[i].innerHTML);
+// 	}
+
+// 	for (var i = 0; i < todos.length; i++) {
+// 		todos[i].onclick = function() {
+// 			liIndex = tab.indexOf(this.innerHTML);
+// 			console.log(this.innerHTML + ' INDEX = ' + liIndex);
+// 		};
+// 	}
+// }
